6cb3ff64e12356205a954e3b8998e6b138fd7b1c
kEND <ken.barker@gmail.com>
2019-12-18 10:12:19 -0500

    Hardcoded feed

--- lib/xenos_podcaster_web/controllers/page_controller.ex
+++ lib/xenos_podcaster_web/controllers/page_controller.ex
@@ -4,4 +4,11 @@ defmodule XenosPodcasterWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def feed(conn, _params) do
+    conn
+    |> put_layout(:none)
+    |> put_resp_content_type("application/xml")
+    |> render("feed.xml")
+  end
 end
--- lib/xenos_podcaster_web/router.ex
+++ lib/xenos_podcaster_web/router.ex
@@ -17,6 +17,7 @@ defmodule XenosPodcasterWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/feed", PageController, :feed
   end
 
   # Other scopes may use custom stacks.
