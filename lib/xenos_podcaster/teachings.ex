9a4b55c39abdb28245130da0ab04add8cd8cd4b6
Ken Barker <ken.barker@gmail.com>
2019-12-18 14:27:49 -0500

    stubbed in real teachings

---
+++ lib/xenos_podcaster/teachings.ex
@@ -0,0 +1,167 @@
+defmodule XenosPodcaster.Teachings do
+  @teachings [
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb1-1.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Introduction",
+     verse_range: "Hebrews 1:1-2:3"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb2-1.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Responding to Revelation",
+     verse_range: "Hebrews 2:1-18"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb3-1.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Saying \"No\" to God",
+     verse_range: "Hebrews 3:1-19"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb5-1.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Avoid Dull Hearing",
+     verse_range: "Hebrews 5:11-6:12"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb5-2.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "What About Priests?",
+     verse_range: "Hebrews 5:1-7:7"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb7-1.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "The New Covenant",
+     verse_range: "Hebrews 7:18-8:13"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb9-1.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Why Rituals?",
+     verse_range: "Hebrews 9:1-12"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb9-2.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Liberation is Complete!",
+     verse_range: "Hebrews 9:13-10:14"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb10-1.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Two Indicatives and Five Imperatives",
+     verse_range: "Hebrews 10:19-31"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb11-1.mp3", 
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "What Is Faith?",
+     verse_range: "Hebrews 11:1-6"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb11-2.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Living Faith From Ancient Lives",
+     verse_range: "Hebrews 11:1-16"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb11-3.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Abraham: Man of Faith",
+     verse_range: "Hebrews 11:8-19"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb11-4.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "The \"Patriarchs\"",
+     verse_range: "Hebrews 11:20-22"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb11-5.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Moses",
+     verse_range: "Hebrews 11:23-29"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb11-6.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "And The Other's Too",
+     verse_range: "Hebrews 11:30-40"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb12-1.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Expanding the Threshold of Faith",
+     verse_range: "Hebrews 12:1-11"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb12-2.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "The Impact of Spiritual Decisions",
+     verse_range: "Hebrews 12:11-29"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb13-1.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Living the Life of Faith",
+     verse_range: "Hebrews 13:1-3"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb13-2.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Living the Life of Faith (Part 2)",
+     verse_range: "Hebrews 13:4"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb13-3.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Living the Life of Faith (Part 3)",
+     verse_range: "Hebrews 13:5-6"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb13-4.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Living the Life of Faith (Part 4)",
+     verse_range: "Hebrews 13:7, 17"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb13-5.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Living the Life of Faith (Part 5)",
+     verse_range: "Hebrews 13:8-9"
+   },
+   %{
+     author: "Dennis McCallum",
+     media_url: "http://services.media.xenos.org/teachings/heb/dennis/2015/heb13-6.mp3",
+     series_title: "Teaching series from  Hebrews",
+     teaching_title: "Living the Life of Faith (Part 6)", 
+     verse_range: "Hebrews 13:12-21"
+   }
+ ]
+
+  def teachings(), do: @teachings
+end
--- lib/xenos_podcaster_web/controllers/page_controller.ex
+++ lib/xenos_podcaster_web/controllers/page_controller.ex
@@ -1,14 +1,16 @@
 defmodule XenosPodcasterWeb.PageController do
   use XenosPodcasterWeb, :controller
+  alias XenosPodcaster.Teachings
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def feed(conn, _params) do
+    teachings = Teachings.teachings
     conn
     |> put_layout(:none)
     |> put_resp_content_type("application/xml")
-    |> render("feed.xml")
+    |> render("feed.xml", teachings: teachings)
   end
 end
