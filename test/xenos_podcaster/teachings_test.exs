dd6b1643bb851ebf79671934cc825a2f453c7043
Ken Barker <ken.barker@gmail.com>
2020-01-06 13:19:52 -0500

    Match author names that are not links

--- test/xenos_podcaster/teachings_test.exs
+++ test/xenos_podcaster/teachings_test.exs
@@ -14,4 +14,12 @@ defmodule XenosPodcaster.TeachingsTest do
     {:ok, %{body: body}} = XenosPodcaster.Scraper.get_teaching_series("346")
     assert Floki.find(body_from_file, "div#content") == Floki.find(body, "div#content")
   end
+
+  test "author from series body" do
+    {:ok, body} = File.read("test/support/245.html")
+    {:ok, body2} = File.read("test/support/346.html")
+
+    assert XenosPodcaster.Teachings.series_author(body) == "Dennis McCallum"
+    assert XenosPodcaster.Teachings.series_author(body2) == "Ben Foust"
+  end
 end
