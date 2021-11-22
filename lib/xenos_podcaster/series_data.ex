65f3d79d29621e72bdb35b4ca7e9c8b6021e553c
Ken Barker <ken.barker@gmail.com>
2021-11-21 21:48:34 -0500

    Map teaching page urls

--- lib/xenos_podcaster/series_data.ex
+++ lib/xenos_podcaster/series_data.ex
@@ -6,16 +6,29 @@ defmodule XenosPodcaster.SeriesData do
     teachings: []
   ]
 
+  alias XenosPodcaster.DwellScraper
+
   def populate(floki_item) do
     ["Book: " <> book | [series]] =
       Floki.find(floki_item, "#active-filters li") |> Floki.text() |> String.split(" Series: ")
 
+    additional_pages = DwellScraper.additional_pages(floki_item)
+
     %__MODULE__{
       series: String.trim(series),
       book: book,
-      teachings_urls: [],
+      teachings_urls: teaching_urls(floki_item) ++ teaching_urls(additional_pages),
       teachings: []
     }
+  end
+
+  def teaching_urls(page_urls) when is_list(page_urls) do
+    page_urls
+    |> Enum.reduce([], fn page_url, teaching_page_urls ->
+      {:ok, %{body: body}} = XenosPodcaster.DwellScraper.get_teaching_page(page_url)
+      teaching_page_urls ++ teaching_urls(body)
+    end)
+    |> List.flatten()
   end
 
   def teaching_urls(body) do
@@ -23,7 +36,7 @@ defmodule XenosPodcaster.SeriesData do
     |> Enum.map(fn x ->
       Floki.find(x, "a")
       |> Floki.attribute("href")
-      |> Floki.text
+      |> Floki.text()
     end)
   end
 end
--- test/xenos_podcaster/series_data_test.exs
+++ test/xenos_podcaster/series_data_test.exs
@@ -70,7 +70,31 @@ defmodule XenosPodcaster.SeriesDataTest do
       expected_series_data = %SeriesData{
         series: "Hebrews by Dennis McCallum (2015)",
         book: "Hebrews",
-        teachings_urls: [],
+        teachings_urls: [
+          "/teaching/3049",
+          "/teaching/3059",
+          "/teaching/3062",
+          "/teaching/3076",
+          "/teaching/3083",
+          "/teaching/3088",
+          "/teaching/3094",
+          "/teaching/3099",
+          "/teaching/3104",
+          "/teaching/3127",
+          "/teaching/3122",
+          "/teaching/3226",
+          "/teaching/3140",
+          "/teaching/3147",
+          "/teaching/3153",
+          "/teaching/3155",
+          "/teaching/3160",
+          "/teaching/3166",
+          "/teaching/3168",
+          "/teaching/3230",
+          "/teaching/3224",
+          "/teaching/3229",
+          "/teaching/3236"
+        ],
         teachings: []
       }
 
@@ -83,6 +107,22 @@ defmodule XenosPodcaster.SeriesDataTest do
 
     test "get urls to teaching pages on summary page" do
       expected_teaching_urls = [
+        "/teaching/1493",
+        "/teaching/1501",
+        "/teaching/1502",
+        "/teaching/1505",
+        "/teaching/3000"
+      ]
+
+      book = "60"
+      series_id = "120"
+      {:ok, %{body: body}} = DwellScraper.get_teaching_series(book, series_id)
+
+      assert SeriesData.teaching_urls(body) == expected_teaching_urls
+    end
+
+    test "get urls to teaching pages from additional pages" do
+      expected_teaching_urls = [
         "/teaching/3049",
         "/teaching/3059",
         "/teaching/3062",
@@ -92,14 +132,30 @@ defmodule XenosPodcaster.SeriesDataTest do
         "/teaching/3094",
         "/teaching/3099",
         "/teaching/3104",
-        "/teaching/3127"
+        "/teaching/3127",
+        "/teaching/3122",
+        "/teaching/3226",
+        "/teaching/3140",
+        "/teaching/3147",
+        "/teaching/3153",
+        "/teaching/3155",
+        "/teaching/3160",
+        "/teaching/3166",
+        "/teaching/3168",
+        "/teaching/3230",
+        "/teaching/3224",
+        "/teaching/3229",
+        "/teaching/3236"
       ]
 
       book = "58"
       series_id = "245"
       {:ok, %{body: body}} = DwellScraper.get_teaching_series(book, series_id)
 
-      assert SeriesData.teaching_urls(body) == expected_teaching_urls
+      additional_pages = DwellScraper.additional_pages(body)
+      teaching_urls = SeriesData.teaching_urls(body)
+
+      assert teaching_urls ++ SeriesData.teaching_urls(additional_pages) == expected_teaching_urls
     end
   end
 end
